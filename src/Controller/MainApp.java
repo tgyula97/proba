97f29190c34f801af0bd0b4f18b30ecd139c13ad
Gyula <gyuszi0097@gmail.com>
2019-04-21 19:22:54 +0200

    ezzeal a szarral

---
+++ src/Controller/MainApp.java
@@ -0,0 +1,47 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Sticky Notes");
+
+        initHomepageLayout();
+
+    }
+
+    public void initHomepageLayout(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/View/homepageLayout.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            Scene scene = new Scene(personOverview);
+            scene.getStylesheets().add("/View/style.css");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage(){
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
